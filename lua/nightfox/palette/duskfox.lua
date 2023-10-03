0968f74436979cbac0aed31a55b93b950c298580
sudorook <daemon@nullcodon.com>
2023-10-03 15:56:18 -0500

    brighten sel0 for Visual selection fg color
    Avoids setting same bg and fg color for each palette and instead keeps
    the fg color similar to the style of the other colors in the palette.

--- lua/nightfox/palette/duskfox.lua
+++ lua/nightfox/palette/duskfox.lua
@@ -33,6 +33,7 @@ local palette = {
   fg3     = "#6e6a86", -- Darker fg (line numbers, fold colums)
 
   sel0    = "#433c59", -- Popup bg, visual selection bg
+  selfg0  = C("#433c59"):brighten(72):to_css(), -- Visual selection fg
   sel1    = "#63577d", -- Popup sel bg, search bg
 }
 
@@ -51,6 +52,7 @@ local function generate_spec(pal)
     fg3  = pal.fg3,  -- Darker fg (line numbers, fold colums)
 
     sel0 = pal.sel0, -- Popup bg, visual selection bg
+    selfg0 = pal.selfg0, -- Visual selection fg
     sel1 = pal.sel1, -- Popup sel bg, search bg
   }
 
